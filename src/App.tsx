ca05418a70b30847c65e25ddf41158d13378f16a
TigrouForMatic <jules.chassany@gmail.com>
2024-02-28 17:59:57 +0100

    Essai d'implementation de la librairie lib-pebble-ui-react pour le menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Sidebar  from  './components/sidebarPebble/SidebarContainerPebble'
 import SquareContainerWeek from './components/periodRender/SquareContainerWeek';
 import {getSquareWeekItems} from './squareContainerWeek'
+// import Sidebar from '@pebble-solutions/lib-pebble-ui-react':
 
 
 function App() {
